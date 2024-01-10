47fa20a78d739660b14c6ac20d3fb7925fb2aa0c
Joel Dunham <jrwdunham@gmail.com>
2024-01-10 09:21:55 -0800

    Only allow create-plan when tier is free

--- src/dvb/server/http/operations/create_plan.clj
+++ src/dvb/server/http/operations/create_plan.clj
@@ -48,21 +48,32 @@
           (log/warn message data)
           (throw (errors/error-code->ex-info :unauthorized)))))))
 
+(defn- validate [plan-write authenticated-user-id]
+  (when-not (= :free (:tier plan-write))
+    (let [message "Attempted to create non-free plan via API call."
+          data {:message message
+                :authenticated-user-id authenticated-user-id
+                :tier (:tier plan-write)
+                :error-code :non-free-plan-creation-attempt}]
+      (log/warn message data)
+      (throw (errors/error-code->ex-info :prohibited-plan-tier)))))
+
 (defn handle
   "Create a new plan. Plan creation entails making the creating user a manager
   of the plan by creating a row in the users_plans table."
   [{:keys [database]} {:as ctx plan-write :request-body}]
   (log/info "Creating a plan.")
   (authorize/authorize ctx)
-  (let [authenticated-user-id (utils/security-user-id ctx)]
+  (let [plan-write (edges/plan-api->clj plan-write)
+        authenticated-user-id (utils/security-user-id ctx)]
     (jdbc/with-db-transaction [tx database]
       (let [user-with-plans (db.users/get-user-with-plans
                              tx authenticated-user-id)]
         (authorize user-with-plans)
-        (let [plan (create-plan-in-db tx (-> plan-write
-                                             edges/plan-api->clj
-                                             (assoc :created-by authenticated-user-id
-                                                    :updated-by authenticated-user-id)))]
+        (validate plan-write authenticated-user-id)
+        (let [plan (create-plan-in-db tx (assoc plan-write
+                                                :created-by authenticated-user-id
+                                                :updated-by authenticated-user-id))]
           (db.user-plans/create-user-plan
            tx {:user-id authenticated-user-id
                :plan-id (:id plan)
